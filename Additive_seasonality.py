22aeeb67501d377c73abd99a9818e7c78303c8d0
Sai Teja <30465745+sai36@users.noreply.github.com>
2020-11-13 11:00:19 +0100

    Create Additive_seasonality.py

---
+++ Additive_seasonality.py
@@ -0,0 +1,41 @@
+from __future__ import division
+from sys import exit
+from math import sqrt
+from numpy import array
+from scipy.optimize import fmin_l_bfgs_b
+import csv
+import pandas as pd
+import psycopg2
+from datetime import datetime
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+
+def additive(x, m, fc, alpha = None, beta = None, gamma = None):
+
+    Y = x[:]
+
+    if (alpha == None or beta == None or gamma == None):
+
+        initial_values = array([0.3, 0.1, 0.1])
+        boundaries = [(0, 1), (0, 1), (0, 1)]
+        type = 'additive'
+
+        parameters = fmin_l_bfgs_b(RMSE, x0 = initial_values, args = (Y, type, m), bounds = boundaries, approx_grad = True)
+        alpha, beta, gamma = parameters[0]
+    a = [sum(Y[0:m]) / float(m)]
+    b = [(sum(Y[m:2 * m]) - sum(Y[0:m])) / float(m ** 2)]
+    s = [Y[i] - a[0] for i in range(m)]
+    y = [a[0] + b[0] + s[0]]
+    rmse = 0
+
+    for i in range(len(Y) + fc):
+
+        if i == len(Y):
+            Y.append(a[-1] + b[-1] + s[-m])
+
+        a.append(alpha * (Y[i] - s[i]) + (1 - alpha) * (a[i] + b[i]))
+        b.append(beta * (a[i + 1] - a[i]) + (1 - beta) * b[i])
+        s.append(gamma * (Y[i] - a[i] - b[i]) + (1 - gamma) * s[i])
+        y.append(a[i + 1] + b[i + 1] + s[i + 1])
+
+    rmse = sqrt(sum([(m - n) ** 2 for m, n in zip(Y[:-fc], y[:-fc - 1])]) / len(Y[:-fc]))
